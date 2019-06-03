ba008fe654c55f04ebbb38bb1358da0f07d8b14e
mickamy <t.mikami1992@gmail.com>
2019-06-03 22:43:57 +0900

    Initial commit

---
+++ Qeader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Qeader
+//
+//  Created by Tetsuro Mikami on 2019/06/03.
+//  Copyright © 2019 Tetsuro Mikami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
